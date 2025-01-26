05fa2ff99faac1d34e77bfc630ff6880313ec526
Hamza-Shahid10 <hamzaasudani@gmail.com>
2025-01-26 14:29:25 +0500

    familiar and unfamiliar operations

---
+++ problem/app.js
@@ -0,0 +1,37 @@
+// let correctPassword = "Saadpaddo"
+
+// let checkPassword = prompt("Enter your password")
+
+// if(checkPassword == null || checkPassword == ""){
+//     alert("enter *Saadpaddo*")
+// } else if (checkPassword == correctPassword){alert('your login succesfully')}
+
+
+// let userName = "admin"
+// let password = "123"
+
+
+// let userInpName = prompt('enter your username')
+// let userInpPassword = prompt('enter your password')
+
+
+// if(userInpPassword == password && userInpName == userName ){
+//     alert("behtreen sahi likhe h sarkar")
+// }else (alert('abay bhsrike parh likh k aa'))
+
+
+
+// if(prompt('enter gender')=='male'){
+//     alert('Welcome sir')} else if (prompt('enter gender')=='female'){
+//         alert('Welcome mam')} else (alert('BECH k ho kia'))
+
+
+
+
+// let c = 12
+
+// if (c++ === 13){alert('this 1 condtion is ture') }; 
+
+// if (c === 13){alert('this 2 condtion is ture') };
+
+// if(++c < 14){alert('this 3 condtion is ture') };
