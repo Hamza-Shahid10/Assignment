60ead657515235e30bbe9e681ba08f3084e746f1
Hamza-Shahid10 <hamzaasudani@gmail.com>
2025-01-31 10:55:42 +0500

    Array ka assignment h

---
+++ Array/app.js
@@ -0,0 +1,55 @@
+const burgerMenu = document.querySelector('.burger-menu');
+    const navLinks = document.querySelector('.nav-links');
+  
+    burgerMenu.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+});
+
+
+
+
+
+
+// Q---1
+// let students = new Array()
+
+// students[0] = 'Hamza Shahid'
+
+// document.write(`
+//   <center>
+//   <select> 
+//   <option>${students[0]}</option>
+//   </select></center>`)
+
+
+// Q---2
+// let devices = new Array()
+
+// devices[0] = 'Computer'
+// devices[1] = 'keyboard'
+// devices[2] = 'Mouse'
+// devices[3] = 'Monitor'
+
+
+// document.write(`
+//   <center>
+//   <select> 
+//   <option>${devices[3]}</option>
+//   <option>${devices[2]}</option>
+//   <option>${devices[1]}</option>
+//   <option>${devices[0]}</option>
+  
+//   </select></center>`
+// )
+
+// Q---3
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// const fruits2 = fruits;
+// fruits.reverse()
+
+
+// document.write(`Score of ${fruits[2]} is ${fruits2[2]}`)
+
+
+
