59bae5d9dfc4081b317b30c52941f7f2ffbe2edd
Hamza Shahid Siddiqui <hamzaasudani@gmail.com>
2025-02-24 10:37:03 +0500

    Date And Time

--- Strings/app.js
+++ Strings/app.js
@@ -45,124 +45,6 @@
 
 
 
-// // Q---14
-// let str = '35.36'
-// let maazBharwa = str.split('.').join('')
-
-// console.log(maazBharwa)
-
-
-// // // Q---15
-// let a = 3
-// let b = '3'
-// let maazBharwa = a + b
-
-
-// console.log(maazBharwa)
-
-
-// Q---17
-
-// let user = prompt('enter name')
-// let arr = ['@',".", ',' , '!']
-
-// for (let i = 0; i < user.length ; i++){
-//   for(let j = 0; j < arr.length; j++ ){
-//     if(user[i] == arr[j]){
-//       console.log('maaz bhrwa likho')
-//     }
-//   }
-// }
-
-
-// Q--18
-// let arr = ['cake',"apple pie", 'cookie']
-// let user = prompt('enter name').toLocaleLowerCase()
-
-// for (let i = 0; i < arr.length ; i++){
-//     if(user == arr[i]){
-//       console.log('maaz bhrwa h')
-
-// }}
-
-// Q--20
-
-// function isValidPassword(password) {
-//   // Check if the password is at least 6 characters long
-//   if (password.length < 6) {
-//     alert("Password must be at least 6 characters long.");
-//     return false;
-//   }
-  
-//   // Check if the password starts with a number
-//   if (/\d/.test(password.charAt(0))) {
-//     alert("Password should not start with a number.");
-//     return false;
-//   }
-  
-//   // Check if the password contains at least one letter and one number
-//   if (!/[a-zA-Z]/.test(password)) {
-//     alert("Password must contain at least one alphabet.");
-//     return false;
-//   }
-  
-//   if (!/[0-9]/.test(password)) {
-//     alert("Password must contain at least one number.");
-//     return false;
-//   }
-  
-//   return true;
-// }
-
-// function promptPassword() {
-//   let password;
-//   // Continue prompting until a valid password is entered
-//   do {
-//     password = prompt("Enter your password:");
-//     // If user clicks cancel, break out of the loop (optional)
-//     if (password === null) {
-//       alert("Password input cancelled.");
-//       return;
-//     }
-//   } while (!isValidPassword(password));
-  
-//   alert("Password is valid.");
-// }
-
-// // Start the prompt
-// promptPassword();
-
-//  Q---21
-// var university = "“University of Karachi”";
-// let newUniversity = university.split(' ')
-//   console.log(newUniversity)
-
-
-  //  Q--22
-
-// let user = 'shafi paddo'
-// let show = user.length - 1
-// let print = user[show]
-// console.log(print)
-
-
-  // Q--23
-let str = 'the quick brown fox jumps over the lazy dog'
-let arr = str.split(' ')
-num = 0
-
-for(let i = 0 ; i < arr.length; i++)
-if(arr[i] == 'the'){
-  num += 1
-}
-
-console.log(num)
-
-let conson = str.length - num
-
-console.log(num, conson)
-
-
   //  Q---24
 // let str = 'pakistan'
 // let arr = str.split('')
@@ -178,3 +60,77 @@ console.log(num, conson)
 // let conson = str.length - num
 
 // console.log(num , conson)
+
+
+
+
+
+
+
+    // date : 17 / 2
+
+
+
+// let a = 5
+// let b = 10
+// let c = 'abc' + a++ - --b - "bcd" + 10 - 'kaisa'
+// let c =  a++ + 'hello'
+
+
+// let c =  'sahi ans' && "false" && "0" && undefined
+ 
+
+// document.write(c)
+
+
+
+
+
+
+
+
+
+// CHAPTER 30
+
+
+// Q---1
+// let num = 19
+// let str = num.toString()
+// console.log(str)
+
+
+
+
+// Q---2
+
+// let num = 15
+// let str = num.toString()
+// if(str.charAt(str.length - 1) === "5") {
+//    str = str.slice(0, str.length - 1) + "6";
+//    }
+
+
+// console.log(str)
+
+
+// let num2 = Number(str) 
+
+// console.log(str, num2)
+
+
+
+
+// NEW CLASS CHATER 31 DATE AND TIME
+// let currentDate  = new Date().toString()
+// document.write(currentDate.slice(16, 24))
+
+
+
+
+
+
+
+
+
+
+
